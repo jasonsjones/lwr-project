35a6f49b79911643e0cfeda19a4dbab3a964a57b
Jason Jones <jsjones96@gmail.com>
2023-09-22 23:56:04 -0700

    test: add jest to project

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+    preset: "@lwc/jest-preset",
+    moduleNameMapper: {
+        "^orionlabs/(.+)$": "<rootDir>/src/modules/orionlabs/$1/$1",
+    },
+    coverageDirectory: "./coverage/jest/",
+    coveragePathIgnorePatterns: ["/node_modules/", ".html", "/__tests__/"],
+};
---
+++ src/modules/orionlabs/app/__tests__/app.spec.js
@@ -0,0 +1,23 @@
+import { createElement } from "lwc";
+import App from "orionlabs/app";
+
+describe("App component", () => {
+    let element;
+
+    beforeEach(() => {
+        element = createElement("orionlabs-app", {
+            is: App,
+        });
+        document.body.appendChild(element);
+    });
+
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it("renders", () => {
+        expect(element).toBeTruthy();
+    });
+});
