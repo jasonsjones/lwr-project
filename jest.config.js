a913e2eafe8d8b02a2b79bedcd9f6e4ab05cd096
Jason Jones <jsjones96@gmail.com>
2023-09-25 08:48:54 -0700

    feat: add simple client-side routing

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 export default {
     preset: '@lwc/jest-preset',
     moduleNameMapper: {
-        '^orionlabs/(.+)$': '<rootDir>/src/modules/orionlabs/$1/$1'
+        '^orionlabs/(.+)$': '<rootDir>/src/modules/orionlabs/$1/$1',
+        '^lwr/(.+)$': '<rootDir>/jest_modules/lwr/$1/$1'
     },
     coverageDirectory: './coverage/jest/',
     coveragePathIgnorePatterns: ['/node_modules/', '.html', '/__tests__/']
--- src/modules/orionlabs/app/app.js
+++ src/modules/orionlabs/app/app.js
@@ -1,5 +1,30 @@
 import { api, LightningElement } from 'lwc';
+import { createRouter } from 'lwr/router';
+
+const routes = [
+    {
+        id: 'home',
+        uri: '/',
+        handler: () => import('orionlabs/homePageHandler'),
+        page: {
+            type: 'home'
+        }
+    },
+    {
+        id: 'namedPage',
+        uri: '/:pageName',
+        handler: () => import('orionlabs/namedPageHandler'),
+        page: {
+            type: 'namedPage',
+            attributes: {
+                pageName: ':pageName'
+            }
+        }
+    }
+];
 
 export default class App extends LightningElement {
+    router = createRouter({ routes });
+
     @api message = 'Hello World!';
 }
