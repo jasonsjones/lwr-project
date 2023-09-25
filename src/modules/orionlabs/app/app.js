1a29c7eae2dfae3a042190173b94e2ee27baa8c0
Jason Jones <jsjones96@gmail.com>
2023-09-25 09:42:18 -0700

    chore: update linting config

--- src/modules/orionlabs/app/app.js
+++ src/modules/orionlabs/app/app.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    @api message = 'Hello World!';
+}
