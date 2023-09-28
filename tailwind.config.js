58fd6bf8dfcd66f98fd8d4e07ad55430b264a54e
Jason Jones <jsjones96@gmail.com>
2023-09-27 21:37:48 -0700

    chore: make dev script concurrent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    content: ['./src/modules/**/*.html', './src/modules/**/*.ts'],
+    content: ['./src/modules/**/*.html', './src/modules/**/*.js'],
     darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {
