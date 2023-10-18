58fc5394fd4dbe68a37a142e3019c23ad2ada564
Jason Jones <jsjones96@gmail.com>
2023-10-17 21:39:57 -0700

    feat: include access token (jwt) with login response

--- packages/api/src/auth/auth-service.mjs
+++ packages/api/src/auth/auth-service.mjs
@@ -1,8 +1,14 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export function verifyPassword(user, password) {
     if (user.password) {
         return bcrypt.compareSync(password, user.password.hash);
     }
     return false;
+}
+
+export function generateAccessToken(user) {
+    const payload = { sub: user.id, email: user.email };
+    return jwt.sign(payload, 'secret', { expiresIn: '10m' });
 }
