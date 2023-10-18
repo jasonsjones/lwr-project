33c048cd83e94270b79defd2fbf9d6c58bb6fa72
Jason Jones <jsjones96@gmail.com>
2023-10-17 21:39:57 -0700

    feat: include access token (jwt) with login response

--- packages/api/src/auth/auth-controller.mjs
+++ packages/api/src/auth/auth-controller.mjs
@@ -1,9 +1,13 @@
+import { generateAccessToken } from './auth-service.mjs';
+
 export async function loginLocal(req, res) {
     const user = req.user;
     if (user) {
+        const accessToken = generateAccessToken(user);
+
         return res.json({
             success: true,
-            accessToken: '<jwt goes here>',
+            accessToken,
             user
         });
     }
