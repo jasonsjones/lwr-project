781439cb1f693054eaf7fafbb9ecd9ecbccdd86f
Jason Jones <jsjones96@gmail.com>
2023-10-22 10:40:29 -0700

    feat: add user list component and /users route

---
+++ packages/app/src/modules/orionlabs/userList/userList.js
@@ -0,0 +1,37 @@
+import { LightningElement, track, wire } from 'lwc';
+import { NavigationContext } from 'lwr/navigation';
+import { getUsers } from 'orionlabs/userApi';
+
+export default class UserList extends LightningElement {
+    @track _users;
+
+    @wire(NavigationContext)
+    navContext;
+
+    @wire(getUsers)
+    getAllUsers({ data, error }) {
+        if (error) {
+            console.log(error.message);
+            return;
+        }
+
+        if (data && Array.isArray(data.users)) {
+            this._users = data.users;
+        }
+    }
+
+    get users() {
+        return (this._users || []).map((user) => {
+            return {
+                ...user,
+                displayName: `${user.firstName} ${user.lastName}`,
+                pageRef: {
+                    type: 'user_detail',
+                    attributes: {
+                        userId: user.id
+                    }
+                }
+            };
+        });
+    }
+}
