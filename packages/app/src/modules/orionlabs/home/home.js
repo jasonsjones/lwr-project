76630244303d83bf090dd765ef7fb947e34476a2
Jason Jones <jsjones96@gmail.com>
2023-10-28 09:27:43 -0700

    refactor: update UX views

--- packages/app/src/modules/orionlabs/home/home.js
+++ packages/app/src/modules/orionlabs/home/home.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class Home extends LightningElement {}
+export default class Home extends LightningElement {
+    get usersPageRef() {
+        return {
+            type: 'namedPage',
+            attributes: {
+                pageName: 'users'
+            }
+        };
+    }
+}
