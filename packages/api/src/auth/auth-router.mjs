6a9d13ad89bcb6d8508d5812d3d881d170e75e1e
Jason Jones <jsjones96@gmail.com>
2023-11-01 19:49:22 -0700

    refactor: remove express api package

--- packages/api/src/auth/auth-router.mjs
+++
@@ -1,9 +0,0 @@
-import Express from 'express';
-import * as AuthController from './auth-controller.mjs';
-import * as AuthMiddleware from './auth-middleware.mjs';
-
-const router = Express.Router();
-
-router.post('/login', AuthMiddleware.authenticateLocal, AuthController.loginLocal);
-
-export default router;
--- packages/api/src/user/user-controller.mjs
+++
@@ -1,6 +0,0 @@
-import * as UserService from './user-service.mjs';
-
-export async function getUsers(_, res) {
-    const users = await UserService.getUsers();
-    return res.json({ users });
-}
