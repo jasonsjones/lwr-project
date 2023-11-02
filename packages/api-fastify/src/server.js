6963aea96d574b9e3710b77767ff211997baab36
Jason Jones <jsjones96@gmail.com>
2023-11-01 17:01:32 -0700

    feat: add POST /auth/login route

--- packages/api-fastify/src/server.js
+++ packages/api-fastify/src/server.js
@@ -1,6 +1,11 @@
 import Fastify from 'fastify';
+import fastifyCookie from '@fastify/cookie';
+import fastifySession from '@fastify/session';
+import fastifyPassport from '@fastify/passport';
+import LocalStrategy from './auth/strategies/local.js';
 import indexRoutes from './index/routes.js';
 import userRoutes from './user/routes.js';
+import authRoutes from './auth/routes.js';
 
 const BASE_URL_V1 = '/api/v1';
 
@@ -18,8 +23,15 @@ export async function buildServer() {
         logger: envToLogger[process.env.NODE_ENV] ?? true
     });
 
+    app.register(fastifyCookie);
+    app.register(fastifySession, { secret: 'super secret session secret ksjkdjoiiiwbnviwuruiowh' });
+    app.register(fastifyPassport.initialize());
+    app.register(fastifyPassport.secureSession());
+    fastifyPassport.use('local', LocalStrategy);
+
     app.register(indexRoutes);
     app.register(userRoutes, { prefix: `${BASE_URL_V1}/users` });
+    app.register(authRoutes, { prefix: `${BASE_URL_V1}/auth` });
 
     return app;
 }
--- packages/api-fastify/src/user/service.js
+++ packages/api-fastify/src/user/service.js
@@ -3,3 +3,7 @@ import prisma from '../db/client.js';
 export async function getUsers() {
     return await prisma.user.findMany();
 }
+
+export async function getUserByEmailIncludePassword(email) {
+    return await prisma.user.findUnique({ where: { email }, include: { password: true } });
+}
