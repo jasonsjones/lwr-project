8befc3b4f9e33e428fd81bc70c9e555358ce0cc3
Jason Jones <jsjones96@gmail.com>
2023-11-02 21:09:08 -0700

    feat: integrate login wire on login form

--- packages/app/src/modules/orionlabs/login/login.js
+++ packages/app/src/modules/orionlabs/login/login.js
@@ -1,15 +1,29 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import { NavigationContext } from 'lwr/navigation';
+import { login } from 'orionlabs/authApi';
 
 export default class Login extends LightningElement {
+    @track error;
+
     @wire(NavigationContext)
     navContext;
 
-    handleLogin(event) {
+    async handleLogin(event) {
         event.preventDefault();
         const email = this.refs.email.value;
         const password = this.refs.password.value;
-        console.log({ email, password });
-        console.log('Login user handler not yet implemented...');
+        if (email.length > 0 && password.length > 0) {
+            try {
+                const _response = await login({ email, password });
+                this.error = '';
+            } catch (err) {
+                this.error = 'Login credentials invalid. Please try again.';
+                this.refs.email.focus();
+            }
+        }
+    }
+
+    get hasError() {
+        return this.error?.length > 0;
     }
 }
