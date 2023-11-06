09e9c53f4b5091baea41a3172f6e995a9e6630ed
Jason Jones <jsjones96@gmail.com>
2023-11-05 21:14:44 -0800

    feat: return refresh token in cookie

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -3,12 +3,15 @@ import fastifyCookie from '@fastify/cookie';
 import fastifySession from '@fastify/session';
 import fastifyPassport from '@fastify/passport';
 import fastifyCors from '@fastify/cors';
+import fastifyEnv from '@fastify/env';
+import { envSchema } from './config/env.schema.js';
 import LocalStrategy from './modules/auth/strategies/local.js';
 import indexRoutes from './modules/index/routes.js';
 import userRoutes from './modules/user/routes.js';
 import authRoutes from './modules/auth/routes.js';
 
 const BASE_URL_V1 = '/api/v1';
+const LOCAL_APP_URL = 'http:localhost:4200';
 
 const envToLogger = {
     development: {
@@ -24,9 +27,13 @@ export async function buildServer() {
         logger: envToLogger[process.env.NODE_ENV] ?? true
     });
 
-    app.register(fastifyCors);
+    app.register(fastifyEnv, { schema: envSchema, dotenv: true });
+    app.register(fastifyCors, {
+        origin: [LOCAL_APP_URL],
+        credentials: true
+    });
     app.register(fastifyCookie);
-    app.register(fastifySession, { secret: 'super secret session secret ksjkdjoiiiwbnviwuruiowh' });
+    app.register(fastifySession, { secret: process.env.SESSION_SECRET });
     app.register(fastifyPassport.initialize());
     app.register(fastifyPassport.secureSession());
     fastifyPassport.use('local', LocalStrategy);
