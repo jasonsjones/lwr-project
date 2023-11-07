6177bae9435d3740220e5dbd16b6b3a4ab4269e9
Jason Jones <jsjones96@gmail.com>
2023-11-07 11:54:50 -0800

    feat: add getContextUser wire adapter

--- packages/wires/src/adapters/adapterAuth.js
+++ packages/wires/src/adapters/adapterAuth.js
@@ -1,6 +1,15 @@
+import { createWireAdapterConstructor } from '@luvio/lwc-luvio';
+import { getContextUserAdapterFactory } from '../generated/adapters/getContextUser';
 import { loginAdapterFactory } from '../generated/adapters/login';
 import { luvio } from '../network';
 
+const getContextUserLuvioAdapter = getContextUserAdapterFactory(luvio);
+const GetContextUserWireAdapter = createWireAdapterConstructor(
+    getContextUserLuvioAdapter,
+    'getContextUser',
+    luvio
+);
+
 const login = loginAdapterFactory(luvio);
 
-export { login };
+export { GetContextUserWireAdapter as getContextUser, login };
