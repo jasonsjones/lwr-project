063306496db956fade0da2f3270cee0645cc51a5
Jason Jones <jsjones96@gmail.com>
2023-11-14 13:50:08 -0800

    chore: add some jsdoc comments for typing info

--- packages/api/src/modules/index/routes.js
+++ packages/api/src/modules/index/routes.js
@@ -1,3 +1,9 @@
+/**
+ * Defines endpoints on the index route that are not otherwise contained
+ * in other modules (e.g., auth, user, etc)
+ *
+ * @param {import('fastify').FastifyInstance} app - the fastify instance
+ */
 async function indexRoutes(app) {
     app.get('/', async function handler() {
         return { message: 'Fastify API root, visit /api/v1/{resource} for more' };
