d0d14ec4479eb513ecddfe6b84fdb3b194a1595f
Jason Jones <jsjones96@gmail.com>
2024-03-23 00:28:37 -0700

    feat: add getUser wire adapter

--- packages/data-service/src/adapters/adapterUser.js
+++ packages/data-service/src/adapters/adapterUser.js
@@ -1,11 +1,15 @@
 import { createWireAdapterConstructor } from '@luvio/lwc-luvio';
 import { createUserAdapterFactory } from '../generated/adapters/createUser';
 import { getUsersAdapterFactory } from '../generated/adapters/getUsers';
+import { getUserAdapterFactory } from '../generated/adapters/getUser';
 import { luvio } from '../transport/network';
 
 const getUsersLuvioAdapter = getUsersAdapterFactory(luvio);
+const getUserLuvioAdapter = getUserAdapterFactory(luvio);
+
 const GetUsersWireAdapter = createWireAdapterConstructor(getUsersLuvioAdapter, 'getUsers', luvio);
+const GetUserWireAdapter = createWireAdapterConstructor(getUserLuvioAdapter, 'getUser', luvio);
 
 const createUser = createUserAdapterFactory(luvio);
 
-export { GetUsersWireAdapter as getUsers, createUser };
+export { GetUsersWireAdapter as getUsers, GetUserWireAdapter as getUser, createUser };
