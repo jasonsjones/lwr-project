22c5f3e57cc43029c9f45b0c95ed55132740c4f2
Jason Jones <jsjones96@gmail.com>
2024-04-06 19:39:17 -0700

    feat: update user detail view

--- packages/app/src/modules/orionlabs/userDetails/userDetails.js
+++ packages/app/src/modules/orionlabs/userDetails/userDetails.js
@@ -1,11 +1,36 @@
 import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lwr/navigation';
+import { getUser } from 'orionlabs/userApi';
 
 export default class UserDetails extends LightningElement {
-    @wire(CurrentPageReference)
-    _pageReference;
+    userId;
+    _user;
 
-    get userId() {
-        return this._pageReference?.attributes?.userId;
+    @wire(getUser, { id: '$userId' })
+    fetchUser({ data, error }) {
+        if (data?.user) {
+            this._user = data.user;
+        }
+    }
+
+    @wire(CurrentPageReference)
+    getPageReference(pageRef) {
+        this.userId = pageRef?.attributes.userId;
+    }
+
+    get firstName() {
+        return this._user?.firstName;
+    }
+
+    get lastName() {
+        return this._user?.lastName;
+    }
+
+    get email() {
+        return this._user?.email;
+    }
+
+    get roles() {
+        return this._user?.roles;
     }
 }
